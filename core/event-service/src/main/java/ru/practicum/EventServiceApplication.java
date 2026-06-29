6314877b42f1dbbf3d5a680d3e7ab693065d5645
Fedor Antonenko <cupofmilk80@gmail.com>
2026-06-29 18:04:23 +0300

    1) добавлен @EntityScan и @EnableJpaRepositories для исключения冲突 из common модуля

--- core/event-service/src/main/java/ru/practicum/EventServiceApplication.java
+++ core/event-service/src/main/java/ru/practicum/EventServiceApplication.java
@@ -2,8 +2,10 @@ package ru.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {
         "ru.practicum.web",
@@ -14,6 +16,17 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 })
 @EnableDiscoveryClient
 @EnableFeignClients(basePackages = "ru.practicum.feign")
+@EntityScan(basePackages = {
+        "ru.practicum.web.category.entity",
+        "ru.practicum.web.event.entity",
+        "ru.practicum.web.admin.entity",
+        "ru.practicum.web.user.entity"
+})
+@EnableJpaRepositories(basePackages = {
+        "ru.practicum.web.category.repository",
+        "ru.practicum.web.event.repository",
+        "ru.practicum.web.admin.repository"
+})
 public class EventServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(EventServiceApplication.class, args);
